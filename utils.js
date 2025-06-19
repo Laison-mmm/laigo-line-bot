82ec028088051f50ae12d0ab5fb144fa83d07221
Laison-mmm <ko8010706@gmail.com>
2025-06-19 21:53:15 +0800

    Delete utils.js

--- utils.js
+++
@@ -1,5 +0,0 @@
-function chineseToNumber(text) {
-  const map = { '一':1, '二':2, '兩':2, '三':3, '四':4, '五':5, '六':6, '七':7, '八':8, '九':9, '十':10 };
-  return map[text] || parseInt(text) || 1;
-}
-module.exports = { chineseToNumber };
