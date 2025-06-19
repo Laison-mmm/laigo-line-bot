db806444896cf9eb60b75262165a302e7d39e0e4
Laison-mmm <ko8010706@gmail.com>
2025-06-19 20:03:48 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -3,3 +3,4 @@ export function normalizePhone(str) {
   if (digits.length === 9) return '0' + digits;
   if (digits.length === 10 && digits.startsWith('0')) return digits;
   return ''; // 防錯：不合法號碼直接丟掉
+}
